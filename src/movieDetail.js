5b403dfcd1dd5243f2467d266379e61c246d9e76
Web2Dev3 <61260404+Web2Dev3@users.noreply.github.com>
2020-12-10 12:58:09 -0600

    Final Update

--- src/movieDetail.js
+++ src/movieDetail.js
@@ -11,15 +11,20 @@ const mystyle = {
      textAlign: "center"
     };
 
+ const [opinion, setOpinion] = React.useState(null);
 return (
 <div style={mystyle}>
 	<h1>{currentMovie.title}</h1>
-	<img src={currentMovie.posterUrl} alt={currentMovie.title}/>
+	<img style={{width:'80%'}} src={currentMovie.posterUrl} alt={currentMovie.title}/>
 	<p>{currentMovie.rating}</p>
 	<p>{currentMovie.director}</p>
 	<p>{currentMovie.releaseYear}</p>
 	<p>{currentMovie.description}</p>
 	<p>{currentMovie.categories.join(', ')}</p>
+ <p>Did you like this Movie?</p>
+<p>{opinion}</p>
+   <Button buttonText={'Yes'} onClickFunction={() => setOpinion('Yes')}/>
+     <Button buttonText={'No'} onClickFunction={() => setOpinion('No')}/>
 	</div>
 );
 
